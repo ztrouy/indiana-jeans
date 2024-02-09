0195ebe5f7b51f71c51848b3f410ebb558ac6655
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-09 13:08:03 -0600

    Added rendering for radio buttons from ownJeansChoices

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
+import { ownJeansChoices } from "./ownsJeans.js"
+
 const container = document.querySelector("#container")
 
 const render = () => {
-    
+    const jeanOwnershipHTML = ownJeansChoices()
+
+    container.innerHTML = jeanOwnershipHTML
 }
+
+render()
