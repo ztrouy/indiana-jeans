1588b137a179fedc67e700882ee234312cf8cc8e
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-09 13:09:49 -0600

    Added an header to HTML generated by ownJeansChoices

--- scripts/ownsJeans.js
+++ scripts/ownsJeans.js
@@ -1,5 +1,6 @@
 export const ownJeansChoices = () => {
-    let html = `<input type="radio" name="ownsJeans" value="true"/> Yes`
+    let html = `<h2>Do you own a pair of blue jeans?</h2>` 
+    html += `<input type="radio" name="ownsJeans" value="true"/> Yes`
     html += `<input type="radio" name="ownsJeans" value="false"/> No`
     return html
 }
