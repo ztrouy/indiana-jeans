27265404383c6cb9a588ca6daf01d664d6bb56c3
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-09 12:57:35 -0600

    created ownJeansChoices function that generates two radio buttons

---
+++ scripts/ownsJeans.js
@@ -0,0 +1,5 @@
+export const ownJeansChoices = () => {
+    let html = `<input type="radio" name="ownsJeans" value="true"/> Yes`
+    html += `<input type="radio" name="ownsJeans" value="false"/> No`
+    return html
+}
