837142a8ad2e38c7d3f48ee2b5e4f06e32c4c34b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-12 13:44:22 -0600

    Added a section tag around the generated HTML

--- scripts/userLocation.js
+++ scripts/userLocation.js
@@ -6,10 +6,12 @@ export const locationTypeChoices = async () => {
     const response = await fetch("http://localhost:8088/socioLocations")
     const locations = await response.json()
 
-    let choicesHTML = "<h2>Which tyoe if area do you live in?</h2>"
+    let choicesHTML = "<section><h2>Which type of area do you live in?</h2>"
     for (const location of locations) {
         choicesHTML += `<input type="radio" name="location" value="${location.id}" /> ${location.label}`
     }
+
+    choicesHTML += "</section>"
 
     return choicesHTML
 }
