816ca39926bb8c2ef4604836f25829a30803f486
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-12 11:13:04 -0600

    Wrote function to convert transient state to permanent state

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -18,3 +18,14 @@ export const setSocioLocationId = (chosenLocation) => {
 
 
 // Function to convert transient state to permanent state
+export const saveSurverySubmission = async () => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(transientState)
+    }
+
+    const response = await fetch("http://localhost:8088/submissions", postOptions)
+}
