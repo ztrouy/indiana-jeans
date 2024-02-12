19a6bf4cc16385bff09979abca622d29853a5e17
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-12 10:09:52 -0600

    Wrote functions to modify transient state

---
+++ scripts/transientState.js
@@ -0,0 +1,18 @@
+// Set up the transient state data structure and provide initial values
+const transientState = {
+    "ownsBlueJeans": false,
+    "socioLocationId": 0
+}
+
+
+// Functions to modify each property of transient state
+export const setOwnsBlueJeans = (chosenOwnership) => {
+    transientState.ownsBlueJeans = chosenOwnership
+}
+
+export const setSocioLocationId = (chosenLocation) => {
+    transientState.socioLocationId = chosenLocation
+}
+
+
+// Function to convert transient state to permanent state
