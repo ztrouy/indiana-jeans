9192a7a43b6acdd6520f659b2d098ead455f6760
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-16 13:51:55 -0600

    Removed unnecessary variable declaration

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -27,7 +27,7 @@ export const saveSurveySubmission = async () => {
         body: JSON.stringify(transientState)
     }
 
-    const response = await fetch("http://localhost:8088/submissions", postOptions)
+    await fetch("http://localhost:8088/submissions", postOptions)
 
     const submissionEvent = new CustomEvent("newSubmissionCreated")
     document.dispatchEvent(submissionEvent)
